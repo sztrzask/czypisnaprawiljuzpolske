a10f1f75ed9e51b9f47eb669180fc2bad4227ec2
Simon T <sztrzaskowski@gmail.com>
2015-10-26 23:42:58 +0100

    wyrzuciłem polymera

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+angular.module('pisApp', [])
+  .controller('SearchController', function ($scope, $http) {
+    var search = this;
+    $scope.lies = [];
+    $http.get('data/data.html').success(function (data) {
+      $scope.lies = data;
+    });
+    $scope.query;
+    $scope.resultLimit = 100;
+  });
