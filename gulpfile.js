02748a23ac1cf846b774d944e320b7a937b97300
Simon T <sztrzaskowski@gmail.com>
2015-10-27 20:02:49 +0100

    Dodałem autoprefixer do gulpa

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 // Less configuration
 var gulp = require('gulp');
 var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('less', function() {
     gulp.src('*/*.less')
         .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
